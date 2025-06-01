fe69a6cf00688c4a44a16ca3da3faa41f57d20c4
tlMark <127731453+tlMark@users.noreply.github.com>
2025-06-02 01:17:08 +0900

    버전 업데이트

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed = 0;
+    public Scanner scanner;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        scanner = GetComponent<Scanner>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    void LateUpdate() 
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+        
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
+}
---
+++ Assets/Code/Reposition.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    Collider2D coll;
+
+    void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Area"))
+        {
+            return;
+        }
+
+        // 플레이어와 오브젝트의 위치 차이 계산
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 myPos = transform.position;
+        Vector3 playerDir = GameManager.instance.player.inputVec;
+
+        float dirX = playerPos.x > myPos.x ? 1 : (playerPos.x < myPos.x ? -1 : 0);
+        float dirY = playerPos.y > myPos.y ? 1 : (playerPos.y < myPos.y ? -1 : 0);
+
+        float diffX = Mathf.Abs(playerPos.x - myPos.x);
+        float diffY = Mathf.Abs(playerPos.y - myPos.y);
+
+        switch (transform.tag)
+        {
+            case "Ground":
+                if (diffX > diffY)
+                {
+                    transform.Translate(Vector3.right * dirX * 40);
+                }
+                else if (diffX < diffY)
+                {
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+            case "Enemy":
+                if(coll.enabled)
+                {
+                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0f));
+                }
+                break;
+        }
+    }
+}
+
+
