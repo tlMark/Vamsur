c8a76eb0f962e28a8e5903e930f861f94dc7f306
tlMark <127731453+tlMark@users.noreply.github.com>
2025-08-04 00:19:33 +0900

    ui작업 둘쨋날
    HP바 구현, 킬 수, 레벨 수 UI에 표현

--- Assets/Code/HUD.cs
+++ Assets/Code/HUD.cs
@@ -7,13 +7,13 @@ public class HUD : MonoBehaviour
 
     public InfoType type;
 
-    Text text;
-    Slider slider;
+    Text tExt;
+    Slider sLider;
 
     void Awake()
     {
-        text = GetComponent<Text>();
-        slider = GetComponent<Slider>();
+        tExt = GetComponent<Text>();
+        sLider = GetComponent<Slider>();
     }
 
     void LateUpdate()
@@ -23,19 +23,24 @@ public class HUD : MonoBehaviour
             case InfoType.EXP:
                 float curexp = GameManager.instance.exp;
                 float maxexp = GameManager.instance.nextExp[GameManager.instance.level];
-                slider.value = curexp / maxexp;
+                sLider.value = curexp / maxexp;
                 break;
             case InfoType.Level:
-
+                tExt.text = string.Format("Lv.{0:F0}", GameManager.instance.level);
                 break;
             case InfoType.Kills:
-
+                tExt.text = string.Format("{0:F0}", GameManager.instance.kill);
                 break;
             case InfoType.Time:
-
+                float reaminTime = GameManager.instance.maxGameTime - GameManager.instance.gameTime;
+                int min = Mathf.FloorToInt(reaminTime / 60);
+                int sec = Mathf.FloorToInt(reaminTime % 60);
+                tExt.text = string.Format("{0:D2}:{1:D2}", min, sec);
                 break;
             case InfoType.Health:
-
+                float curHealth = GameManager.instance.health;
+                float maxHealth = GameManager.instance.maxHealth;
+                sLider.value = curHealth / maxHealth;
                 break;
         }
     }
